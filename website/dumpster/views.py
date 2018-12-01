5e0fc048be39ba17f5b75df37f1894445ddd8912
Henry Gilbert <henrygilbert15@live.co.uk>
2018-12-01 12:54:04 +0000

    Add listing all of the test user's games

--- website/dumpster/views.py
+++ website/dumpster/views.py
@@ -1,20 +1,20 @@
 from django.shortcuts import redirect, render
 from django.views import View
+from .sample_json import user_game_list_sample
 
 # Create your views here.
 def index(request):
     return render(request, 'dumpster/index.html')
 
 
-def select_game(request, steam_id):
-    return render(request, 'dumpster/index.html')
-
-
 class InputSteamURL(View):
     def get(self, request):
-        print("GETTING HERE")
         return render(request, 'dumpster/input_user.html')
 
     def post(self, request):
-        print("POSTING HERE")
         return redirect('select_game', steam_id='0123456789')
+
+
+def select_game(request, steam_id):
+    context = {"games_json": user_game_list_sample["response"]["games"]}
+    return render(request, 'dumpster/select_game.html', context)
