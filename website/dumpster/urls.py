55856a9ec5761245fbda56cf4d816bfda3ea6e79
Henry Gilbert <henrygilbert15@live.co.uk>
2018-12-01 12:36:21 +0000

    Setup stub for select game page

--- website/dumpster/urls.py
+++ website/dumpster/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('selectuser', views.inputuser)
+    path('selectuser', views.InputSteamURL.as_view()),
+    path('selectgame/<int:steam_id>', views.select_game, name='select_game'),
 ]
--- website/dumpster/views.py
+++ website/dumpster/views.py
@@ -1,9 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.views import View
 
 # Create your views here.
 def index(request):
     return render(request, 'dumpster/index.html')
 
 
-def inputuser(request):
-    return render(request, 'dumpster/input_user.html')
+def select_game(request, steam_id):
+    return render(request, 'dumpster/index.html')
+
+
+class InputSteamURL(View):
+    def get(self, request):
+        print("GETTING HERE")
+        return render(request, 'dumpster/input_user.html')
+
+    def post(self, request):
+        print("POSTING HERE")
+        return redirect('select_game', steam_id='0123456789')
