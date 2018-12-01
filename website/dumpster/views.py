0fb6d6611e0d7e1b2913ebffa0b8b0d81b806611
Henry Gilbert <henrygilbert15@live.co.uk>
2018-12-01 11:43:20 +0000

    Add background task setup

--- website/dumpster/views.py
+++ website/dumpster/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from background_task import background
 
 # Create your views here.
 def index(request):
     return render(request, 'dumpster/index.html')
+
+
+def starttasks(request):
+    print("Adding hook task")
+    task_check_hooks(repeat=10)
+    return JsonResponse({})
+
+
+@background(schedule=10)
+def task_check_hooks():
+    print("HOOK CHECKS HERE")
