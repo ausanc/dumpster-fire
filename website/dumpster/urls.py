c890a59a340479ef54ba64f494d109c68b936dce
Henry Gilbert <henrygilbert15@live.co.uk>
2018-12-01 14:27:57 +0000

    Add make_new_hook view

--- website/dumpster/urls.py
+++ website/dumpster/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('selectgame/<int:steam_id>', views.select_game, name='select_game'),
     path('selectachieve/<int:steam_id>/<int:app_id>',
          views.select_achieve, name='select_achieve'),
+    path('makenewhook', views.make_new_hook, name='make_new_hook'),
+    path('hooks', views.view_hooks)
 ]
--- website/dumpster/views.py
+++ website/dumpster/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import redirect, render
 from django.views import View
 from .sample_json import user_game_list_sample, user_stats_example, names
+from .models import Hook
+from django.http import JsonResponse
+from django.core import serializers
 
 app_id_to_name = {}
 for appinfo in names["applist"]["apps"]:
@@ -41,3 +44,23 @@ def select_achieve(request, steam_id, app_id):
         "app_id": app_id,
     }
     return render(request, 'dumpster/select_achievement.html', context)
+
+
+def make_new_hook(request):
+    if request.method == 'POST':
+        print("MAKING NEW HOOK")
+        steam_id = request.POST.get('steamID')
+        app_id = request.POST.get('appID')
+        achievement_name = request.POST.get('achievementName')
+        print(steam_id, app_id, achievement_name)
+
+        hook = Hook(account_url=steam_id, game_id=app_id, achievement_id=achievement_name)
+        hook.save()
+
+    return render(request, 'dumpster/index.html')
+
+
+def view_hooks(request):
+    qs = Hook.objects.all()
+    qs_json = serializers.serialize('json', qs)
+    return JsonResponse(qs_json, safe=False)
