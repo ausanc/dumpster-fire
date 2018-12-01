7027deca02724378de2e817da4cc09163323a54b
Henry Gilbert <henrygilbert15@live.co.uk>
2018-12-01 12:13:57 +0000

    Add basic URL input

--- website/dumpster/views.py
+++ website/dumpster/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'dumpster/index.html')
+
+
+def inputuser(request):
+    return render(request, 'dumpster/input_user.html')
